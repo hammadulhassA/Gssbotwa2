c4f831dd04d32628c893edbf3f6a95abef417274
hammadulhassA <150794035+hammadulhassA@users.noreply.github.com>
2024-06-03 20:42:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,25 +7,25 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
+global.ownername = "QUEEN";
+global.owner = ['923136180115'];
 global.premium = ['917050906659'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
 global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
-global.prefa = [".", "!", "/", "?"];
+global.prefa = [">", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
-global.autoread = false
-global.available = false
-global.autoTyping = false
+global.autoread = true
+global.available = true
+global.autoTyping = true
 global.autoRecord = false
-global.autoreact = false
+global.autoreact = true
 global.onlygroup = false
 global.onlypc = false
 global.welcome = true
-global.autoBlock = true
+global.autoBlock = false
  
 /*REPLY MESSAGE*/
 global.mess = {
